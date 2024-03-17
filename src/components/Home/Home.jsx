1f68ef46bb8fa16e74b03ae88bafa5765d1df6c6
devashishsoni98 <devashishsoni926@gmail.com>
2024-03-17 17:59:46 +0530

    home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -13,8 +13,8 @@ const Home = () => {
             <button className="home-btn">
               <Link to="/">
                 <span className="flex">
-               Explore Products 
-              <FaArrowRightLong className="home-icon"/>
+                  Explore Products
+                  <FaArrowRightLong className="home-icon" />
                 </span>
               </Link>
             </button>
@@ -24,18 +24,87 @@ const Home = () => {
           <div className="home-container-2">
             <div className="grid-2">
               <div className="grid-2-h">
-                <h1>ArtisianVista is a fashion platform to shop from a wide range of brand products for everyone</h1>
+                <h1>
+                  ArtisianVista is a fashion platform to shop from a wide range
+                  of brand products for everyone
+                </h1>
               </div>
+
               <div className="home-cards-container">
                 <div className="home-grid">
                   <div className="h-c-item">
                     <Link to="/" className="c-link">
-                      
+                      <div className="c-img">
+                        <img src="computer.webp" alt="shirt" />
+                      </div>
+                      <div className="c-title">Lorem ipsum dolor sit amet.</div>
+                      <div className="c-price">&#8377; 350</div>
+                    </Link>
+                  </div>
+
+                  <div className="h-c-item">
+                    <Link to="/" className="c-link">
+                      <div className="c-img">
+                        <img src="mobile.avif" alt="shirt" />
+                      </div>
+                      <div className="c-title">Lorem ipsum dolor sit amet.</div>
+                      <div className="c-price">&#8377; 350</div>
+                    </Link>
+                  </div>
+
+                  <div className="h-c-item">
+                    <Link to="/" className="c-link">
+                      <div className="c-img">
+                        <img src="laptop.jpg" alt="shirt" />
+                      </div>
+                      <div className="c-title">Lorem ipsum dolor sit amet.</div>
+                      <div className="c-price">&#8377; 350</div>
+                    </Link>
+                  </div>
+
+                  <div className="h-c-item">
+                    <Link to="/" className="c-link">
+                      <div className="c-img">
+                        <img src="spices.jpg" alt="shirt" />
+                      </div>
+                      <div className="c-title">Lorem ipsum dolor sit amet.</div>
+                      <div className="c-price">&#8377; 350</div>
+                    </Link>
+                  </div>
+
+                  <div className="h-c-item">
+                    <Link to="/" className="c-link">
+                      <div className="c-img">
+                        <img src="notebooks.jpg" alt="shirt" />
+                      </div>
+                      <div className="c-title">Lorem ipsum dolor sit amet.</div>
+                      <div className="c-price">&#8377; 350</div>
+                    </Link>
+                  </div>
+
+                  <div className="h-c-item">
+                    <Link to="/" className="c-link">
+                      <div className="c-img">
+                        <img src="shoes.jpg" alt="shirt" />
+                      </div>
+                      <div className="c-title">Lorem ipsum dolor sit amet.</div>
+                      <div className="c-price">&#8377; 350</div>
                     </Link>
                   </div>
                 </div>
               </div>
             </div>
+          </div>
+          <div className="home-grid more-products">
+            <div className="divider"></div>
+            <Link to="/" className="h-btn-2">
+              {" "}
+              <span className="flex gap-3">
+                {" "}
+                More Products <FaArrowRightLong className="mp" />{" "}
+              </span>
+            </Link>
+            <div className="divider"></div>
           </div>
         </div>
       </div>
